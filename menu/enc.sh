8a8efb50e844fa2b19be9d3c7e6c795b086dfe3b
LynzXvA <143097592+LynzXvA@users.noreply.github.com>
2023-08-27 23:50:39 +0800

    Update enc.sh

--- menu/enc.sh
+++ menu/enc.sh
@@ -18,7 +18,7 @@ echo "
   ┳━┓  ┳━┓  ┓━┓  ┳ ┳    ┳━┓  ┏━┓  ┏━┓  ┳┏ 
   ┃━┃  ┃━┫  ┗━┓  ┃━┫    ┃┳┛  ┃ ┃  ┃    ┣┻┓
   ┇━┛  ┛ ┇  ━━┛  ┇ ┻    ┇┗┛  ┛━┛  ┗━┛  ┇ ┛" | lolcat -p 0.7
-echo -e "                               by-$BOLD rouze_d$STAND"
+echo -e "                               by-$BOLD LynzXvA$STAND"
 echo -e "$BLUE Encrypt your BASH shell/script"
 #echo -e "$GREEN$BOLD Encrypt$STAND your$GREEN$BOLD BASH$STAND script"
 #echo -e "$RED$BOLD Encode your bash script$STAND"
